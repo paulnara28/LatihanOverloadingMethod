787ac453b027c6e1c5ec883955974d1a9bc54a00
paul <paulnara@students.amikom.ac.id>
2022-04-09 19:09:35 +0800

    inisialisasi project

---
+++ LatihanOverloadingMethod/Numbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LatihanOverloadingMethod
+{
+	public class Numbers
+	{
+		public int FindMinimum(int number1, int number2)
+		{
+			return  number2;
+		}
+		public int FindMinimum(int number1, int number2, float number3)
+		{
+			return number2;
+		}
+		public int FindMaximum(int number1, int number2)
+		{
+			return number1;
+		}
+		public int FindMaximum(int number1, int number2, int number3)
+		{
+			return number3;
+		}
+	}
+}
---
+++ LatihanOverloadingMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMethod
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Numbers number = new Numbers();
+
+			Console.WriteLine("Minimum #1: {0}", number.FindMinimum(3,1));
+			Console.WriteLine("Minimum #2: {0}", number.FindMinimum(3, 2, 4));
+			Console.WriteLine(); //tambahkan baris kosong
+			Console.WriteLine("Maximum #3: {0}", number.FindMaximum(3, 1));
+			Console.WriteLine("Maximum #4: {0}", number.FindMaximum(3, 2, 4));
+			
+			Console.ReadKey();
+		}
+		static void number (string[] args)
+        {
+			Console.WriteLine();
+		}
+	}
+}
